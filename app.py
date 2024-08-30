11aeb079b6f6af91fe5a7e7f5a829a0f99a3ba79
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-08-30 10:21:25 -0600

    generally flailing atm

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import ndf_parse as ndf
 
 app = FastAPI()
 
@@ -6,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/csv")
+async def export_csv(src_path: str, dst_path: str):
+    """
+    Exports the files in the selected folder (including subfolders) as CSVs organized by namespace and type, for analysis
+    
+    `src_path`: a `str` pointing to the local folder holding the files to export
+    `dst_path`: a `str` pointing to a folder where the CSVs will be placed
+    """
+    return {"message": "not implemented"}
