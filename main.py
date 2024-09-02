814942de48918866dc2c58f99d9d9c290e71590a
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-02 08:44:32 -0600

    change pack_list to actual dict; add dict_to_map

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import ndf_parse as ndf
 from DivisionCreationContext import DivisionCreationContext
 from metadata import DivisionMetadata, ModMetadata
+from ndf_utils import dict_to_map
 
 mod_metadata = ModMetadata('dninemfive', '9th Infantry Division (Motorized)', r'C:/Program Files (x86)/Steam/steamapps/common/WARNO/Mods/', "0.0.0")
 div_metadata = DivisionMetadata('d9', '9ID', 'US', 1390)
@@ -18,48 +19,47 @@ guid_cache_path: str = "guid_cache.txt"
 
 # make packlist
 pack_list: dict[str, int] = {
-    ('~/Descriptor_Deck_Pack_AH1F_ATAS_US', 2),
-    ('~/Descriptor_Deck_Pack_AH1F_Cobra_US', 4),
-    ('~/Descriptor_Deck_Pack_AH1S_Cobra_US', 4),
-    ('~/Descriptor_Deck_Pack_AH64_Apache_US', 2),
-    ('~/Descriptor_Deck_Pack_ATteam_TOW2_US', 2),
-    ('~/Descriptor_Deck_Pack_Airborne_CMD_US', 1),
-    ('~/Descriptor_Deck_Pack_Airborne_Dragon_US', 2),
-    ('~/Descriptor_Deck_Pack_Airborne_Scout_US', 1),
-    ('~/Descriptor_Deck_Pack_CH47_Super_Chinook_US', 2),
-    ('~/Descriptor_Deck_Pack_EH60A_EW_US', 1),
-    ('~/Descriptor_Deck_Pack_Engineer_CMD_US', 2),
-    ('~/Descriptor_Deck_Pack_FOB_US', 1),
-    ('~/Descriptor_Deck_Pack_Howz_M102_105mm_US', 2),
-    ('~/Descriptor_Deck_Pack_Howz_M198_155mm_US', 2),
-    ('~/Descriptor_Deck_Pack_LRRP_US', 1),
-    ('~/Descriptor_Deck_Pack_M577_US', 1),
-    ('~/Descriptor_Deck_Pack_M981_FISTV_US', 1),
-    ('~/Descriptor_Deck_Pack_MANPAD_Stinger_C_US', 1),
-    ('~/Descriptor_Deck_Pack_Mortier_107mm_US', 2),
-    ('~/Descriptor_Deck_Pack_OH58C_CMD_US', 1),
-    ('~/Descriptor_Deck_Pack_OH58C_Scout_US', 2),
-    ('~/Descriptor_Deck_Pack_OH58D_Combat_Scout_US', 1),
-    ('~/Descriptor_Deck_Pack_Ranger_Dragon_US', 1),
-    ('~/Descriptor_Deck_Pack_Rifles_Cavalry_US', 1),
-    ('~/Descriptor_Deck_Pack_Rifles_half_AT4_US', 1),
-    ('~/Descriptor_Deck_Pack_Rifles_half_CMD_US', 1),
-    ('~/Descriptor_Deck_Pack_Rifles_half_Dragon_US', 4),
-    ('~/Descriptor_Deck_Pack_Sniper_US', 1),
-    ('~/Descriptor_Deck_Pack_UH60A_CO_US', 1),
-    ('~/Descriptor_Deck_Pack_UH60A_Supply_US', 1),
+    '~/Descriptor_Deck_Pack_AH1F_ATAS_US': 2,
+    '~/Descriptor_Deck_Pack_AH1F_Cobra_US': 4,
+    '~/Descriptor_Deck_Pack_AH1S_Cobra_US': 4,
+    '~/Descriptor_Deck_Pack_AH64_Apache_US': 2,
+    '~/Descriptor_Deck_Pack_ATteam_TOW2_US': 2,
+    '~/Descriptor_Deck_Pack_Airborne_CMD_US': 1,
+    '~/Descriptor_Deck_Pack_Airborne_Dragon_US': 2,
+    '~/Descriptor_Deck_Pack_Airborne_Scout_US': 1,
+    '~/Descriptor_Deck_Pack_CH47_Super_Chinook_US': 2,
+    '~/Descriptor_Deck_Pack_EH60A_EW_US': 1,
+    '~/Descriptor_Deck_Pack_Engineer_CMD_US': 2,
+    '~/Descriptor_Deck_Pack_FOB_US': 1,
+    '~/Descriptor_Deck_Pack_Howz_M102_105mm_US': 2,
+    '~/Descriptor_Deck_Pack_Howz_M198_155mm_US': 2,
+    '~/Descriptor_Deck_Pack_LRRP_US': 1,
+    '~/Descriptor_Deck_Pack_M577_US': 1,
+    '~/Descriptor_Deck_Pack_M981_FISTV_US': 1,
+    '~/Descriptor_Deck_Pack_MANPAD_Stinger_C_US': 1,
+    '~/Descriptor_Deck_Pack_Mortier_107mm_US': 2,
+    '~/Descriptor_Deck_Pack_OH58C_CMD_US': 1,
+    '~/Descriptor_Deck_Pack_OH58C_Scout_US': 2,
+    '~/Descriptor_Deck_Pack_OH58D_Combat_Scout_US': 1,
+    '~/Descriptor_Deck_Pack_Ranger_Dragon_US': 1,
+    '~/Descriptor_Deck_Pack_Rifles_Cavalry_US': 1,
+    '~/Descriptor_Deck_Pack_Rifles_half_AT4_US': 1,
+    '~/Descriptor_Deck_Pack_Rifles_half_CMD_US': 1,
+    '~/Descriptor_Deck_Pack_Rifles_half_Dragon_US': 4,
+    '~/Descriptor_Deck_Pack_Sniper_US': 1,
+    '~/Descriptor_Deck_Pack_UH60A_CO_US': 1,
+    '~/Descriptor_Deck_Pack_UH60A_Supply_US': 1,
     # add new units here...
 }
 
-with DivisionCreationContext(mod, guid_cache_path) as context:
-    context.make_division(mod_name_internal,
-                          copy_of = "Descriptor_Deck_Division_US_82nd_Airborne_multi",
+with DivisionCreationContext(mod, div_metadata, guid_cache_path) as context:
+    context.make_division(copy_of = "Descriptor_Deck_Division_US_82nd_Airborne_multi",
                           DescriptionHintTitleToken = "'ECGMWQOEZA'",
-                          EmblemTexture = '"Texture_Division_Emblem_US_35th_infantry_division"')
+                          EmblemTexture = '"Texture_Division_Emblem_US_35th_infantry_division"',
+                          PackList = dict_to_map(pack_list))
     # make new units
     """ LOG """
     # M998 HUMVEE SUPPLY
-    context.make_unit()
     # M1075 PLS
     # (copy HEMTT, but with higher base XP)
     # ✪ M998 HUMVEE SGT.
--- ndf_utils.py
+++ ndf_utils.py
@@ -1,4 +1,4 @@
-from ndf_parse.model import Object
+from ndf_parse.model import Map, MapRow, Object
 from ndf_parse.model.abc import CellValue
 
 def edit_member(obj: Object, name: str, value: CellValue | None):
@@ -8,3 +8,9 @@ def edit_member(obj: Object, name: str, value: CellValue | None):
 def edit_members(obj: Object, **kwargs: CellValue | None):
     for k, v in kwargs.items():
         edit_member(obj, k, v)
+
+def dict_to_map(input: dict) -> Map:
+    result = Map()
+    for k, v in input.items():
+        result.add(MapRow((k, str(v))))
+    return result
