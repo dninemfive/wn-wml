ade95143a896ba0e93f1fca2b37729cb8650ddd9
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-09 20:01:49 -0600

    add division icon

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Adds the 9th Infantry Division in its High Technology Light Division form as a new division in WARNO. See [this document](https://docs.google.com/document/d/1fDYeJVuQjQBt5jIw4dwWt4pdex7MJqqnqmyFx7WD-Os/edit?usp=sharing) for a summary of the research used to assemble this list. 
 
 Uses [ndf-parse](https://github.com/Ulibos/ndf-parse) to make updating less tedious. i intend to make a framework which i will pull out to make definitions even easier.
+
+# Credits
+- Division icon is the [9th Infantry Division shoulder patch](https://commons.wikimedia.org/wiki/File:9th_Infantry_Division_patch.svg) found on Wikimedia Commons, created by Steven Williamson and placed in the public domain as a government work product.
--- script/ndf_paths.py
+++ script/ndf_paths.py
@@ -6,5 +6,6 @@ DIVISION_PACKS          = rf'GameData\Generated\Gameplay\Decks\DivisionPacks.ndf
 DIVISION_RULES          = rf"GameData\Generated\Gameplay\Decks\DivisionRules.ndf"
 SHOWROOM_EQUIVALENCE    = rf'GameData\Generated\Gameplay\Gfx\ShowRoomEquivalence.ndf'
 UNITE_DESCRIPTOR        = rf'GameData\Generated\Gameplay\Gfx\UniteDescriptor.ndf'
+DIVISION_TEXTURES       = rf'GameData\Generated\UserInterface\Textures\DivisionTextures.ndf'
 
-ALL = set([ALL_UNITS_TACTIC, DECK_SERIALIZER, DIVISIONS, DIVISION_LIST, DIVISION_PACKS, DIVISION_RULES, SHOWROOM_EQUIVALENCE, UNITE_DESCRIPTOR])
+ALL = set([ALL_UNITS_TACTIC, DECK_SERIALIZER, DIVISIONS, DIVISION_LIST, DIVISION_PACKS, DIVISION_RULES, SHOWROOM_EQUIVALENCE, UNITE_DESCRIPTOR, DIVISION_TEXTURES])
