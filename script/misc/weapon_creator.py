e21ad771bc3d107ff0be0864dab06bcd03c892a8
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-10 13:51:59 -0600

    start working on adding weapons and ammo

---
+++ script/misc/weapon_creator.py
@@ -0,0 +1,56 @@
+# right, python is stupid so i can't use type hints for this
+# from context.unit_creation_context import UnitCreationContext
+from context.module_context import ModuleContext
+from message import Message
+from metadata.unit import UnitMetadata
+from ndf_parse import Mod
+from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object
+from ndf_parse.model.abc import CellValue
+from ndf_paths import AMMUNITION
+from typing import Self
+from utils.ndf import edit_members, ndf_path, get_module, replace_unit_module, remove_module
+
+class WeaponCreator(object):
+    def __init__(self: Self, ctx, unit: UnitMetadata, copy_of: str):
+        self.ctx = ctx
+        self.name = f'WeaponDescriptor_{unit.name}'
+        self.copy_of = copy_of
+
+    def __enter__(self: Self) -> Self:
+        self.root_msg = self.ctx.root_msg.nest(f"Making {self.name}")
+        self.root_msg.__enter__()
+        with self.root_msg.nest(f"Copying {self.copy_of}") as _:
+            self.unit_object = self.make_copy(self.ctx.ndf[AMMUNITION])
+        return self
+    
+    def __exit__(self: Self, exc_type, exc_value, traceback):
+        self.apply(self.ctx.ndf, self.root_msg)
+        self.root_msg.__exit__(exc_type, exc_value, traceback)
+
+    def apply(self: Self, ndf: dict[str, List], msg: Message):
+        with msg.nest(f"Saving {self.new.name}") as msg2:
+            self.edit_ammunition(ndf, msg2)
+
+    def make_copy(self: Self, ndf: List) -> Object:
+        copy: Object = ndf.by_name(self.src.descriptor_name).value.copy()
+        edit_members(copy,
+                     DescriptorId=self.ctx.generate_guid(self.name))
+        # TODO: update HitRollRuleDescriptor guid
+        return copy
+
+    @ndf_path(AMMUNITION)
+    def edit_ammunition(self: Self, ndf: List):
+        ndf.add(ListRow(self.unit_object, namespace=self.name, visibility="export"))
+
+    def edit_members(self: Self, **kwargs: CellValue):
+        edit_members(self.unit_object, **kwargs)
+
+    def module_context(self: Self, module_type: str) -> ModuleContext:
+        return ModuleContext(self.unit_object, module_type)
+
+    def get_module(self: Self, module_type: str) -> Object:
+        result: Object | None = get_module(self.unit_object, module_type)
+        return result
+    
+    def remove_module(self: Self, module_type: str) -> None:
+        remove_module(self.unit_object, module_type)
---
+++ script/units/xm142_himars_clu.py
@@ -0,0 +1,31 @@
+from context.module_context import ModuleContext
+from context.unit_creation_context import UnitCreationContext
+from metadata.division_unit_registry import UnitInfo
+from metadata.unit import UnitMetadata
+from misc.unit_creator import UNIT_UI
+from misc.ammo_creator import AmmoCreator
+from ndf_parse.model import List, ListRow
+from utils.ndf import to_List as qlist
+
+def create(ctx: UnitCreationContext) -> UnitInfo | None:
+    # M198 155mm COPPERHEAD
+    # copy M198 155mm
+    with ctx.create_unit("XM142 HIMARS [CLU]", "US", "lars probably idk") as m198_clu:
+        # copy MLRS ammo but with 6 instead of 12 shots
+        with AmmoCreator(ctx, "RocketArt_M26_227mm_Cluster_HIMARS") as ammo:
+            ammo.edit_members(NbTirParSalves=6,
+                              AffichageMunitionParSalve=6)
+        with m198_clu.module_context('TBaseDamageModuleDescriptor') as damage_module:
+            # TODO: dynamically adjust this from M198 
+            damage_module.edit_members(MaxPhysicalDamages=7)
+        # update transportable (TODO: automate this)
+        with m198_clu.module_context('TTransportableModuleDescriptor') as transportable_module:
+            transportable_module.edit_members(TransportedSoldier='"d9_M198_COPPERHEAD_US"')
+        # upgrade from M198 [CLU]
+        with m198_clu.module_context('TUnitUIModuleDescriptor') as ui_module:
+            ui_module.edit_members(UpgradeFromUnit="Descriptor_Unit_d9_M198_155mm_CLU_US")
+        # change unit dangerousness (see 2S3M1 vs regular)
+        # change unit attack/defense value (see 2S3M1 vs regular)
+        # change unit cost (see 2S3M1 vs regular)
+        return UnitInfo(m198_clu, 2, [0, 4, 3, 0])
+        
