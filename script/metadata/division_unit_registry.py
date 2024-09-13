3783b71fd28a17d520bf99b0073b43e1287f6299
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-13 13:13:35 -0600

    don't register a unit as transportless if it has transports

--- script/metadata/division_unit_registry.py
+++ script/metadata/division_unit_registry.py
@@ -10,6 +10,15 @@ from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object
 from typing import Self
 from utils.ndf import map_from_rows, make_obj, to_List
 
+def ensure_unit_path_list(transports: str | list[str] | None) -> list[str] | None:
+        if transports is None:
+            return None
+        if isinstance(transports, str):
+            transports = [transports]
+        def ensure_path(descriptor: str) -> str:
+            return descriptor if descriptor.startswith("$/GFX/Unit/") else f'$/GFX/Unit/{descriptor}'
+        return [ensure_path(x) for x in transports]
+
 class DivisionUnitRegistry(object):
 
     def __init__(self: Self, lookup: DivisionRuleLookup, parent_msg: Message | None = None, *units: UnitInfo):
@@ -20,18 +29,17 @@ class DivisionUnitRegistry(object):
         self.parent_msg = parent_msg
 
     def register(self: Self, info: UnitInfo, override_transports: str | list[str] | None = None):
-        if isinstance(override_transports, str):
-            override_transports = [override_transports]
+        override_transports = ensure_unit_path_list(override_transports)
         if override_transports is not None:
             info.rule.AvailableTransportList = override_transports
+            info.rule.AvailableWithoutTransport = False
         with try_nest(self.parent_msg, f"Registering {info.unit.name}") as _:
             self.units.append(info)
 
     def register_vanilla(self: Self, unit: str | UnitMetadata, packs: int, override_transports: str | list[str] | None = None):
         if isinstance(unit, str):
             unit = UnitMetadata(unit)
-        if isinstance(override_transports, str):
-            override_transports = [override_transports]
+        override_transports = ensure_unit_path_list(override_transports)
         with try_nest(self.parent_msg, f"Registering vanilla unit {unit}") as msg:
             unite_rule = self.lookup.look_up(unit.descriptor_path, override_transports)
             if unite_rule is not None:
