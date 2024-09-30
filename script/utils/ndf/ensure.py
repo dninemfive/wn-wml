6bf613dbcb50447a9f38a16448a1d9dd141aa1ed
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-09-30 12:38:22 -0600

    add autogenerated UnitUIModule members

--- script/utils/ndf/ensure.py
+++ script/utils/ndf/ensure.py
@@ -4,7 +4,7 @@ from typing import Iterable, Literal, Type
 from typing import get_args as literal_values
 from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object, Template
 from ndf_parse.model.abc import CellValue
-from constants.primitive_types import CountryCode, CountrySoundCode
+from constants.primitive_types import MotherCountry
 from constants.ndf import COUNTRY_CODE_TO_COUNTRY_SOUND_CODE
 
 def listrow(val: CellValue | ListRow) -> ListRow:
@@ -160,8 +160,8 @@ def literal(s: str, *literal_types):
     assert s in valid_values, f"{s} is not one of {valid_values}"
     return quoted(s, "'")
 
-def country_sound_code(country: str) -> CountrySoundCode:
-    country = literal(country, CountryCode, CountrySoundCode)
+def country_sound_code(country: str) -> str:
+    country = MotherCountry.ensure_valid(country)
     if country in COUNTRY_CODE_TO_COUNTRY_SOUND_CODE:
         return COUNTRY_CODE_TO_COUNTRY_SOUND_CODE[country]
     return country
--- script/wrappers/unit_modules/unit_ui.py
+++ script/wrappers/unit_modules/unit_ui.py
@@ -1,6 +1,8 @@
 from typing import Self
 from wrappers.unit_modules._abc import UnitModuleWrapper
 from ndf_parse.model import Object
+from constants.primitive_types import UnitRole
+import utils.ndf.edit as edit
 
 class UnitUiModuleWrapper(UnitModuleWrapper):
     def __init__(self: Self, ctx, obj: Object):
@@ -8,4 +10,89 @@ class UnitUiModuleWrapper(UnitModuleWrapper):
         self.object = obj
 
     @property
-    def UnitRole(self: Self) -> UnitRole:
+    def ButtonTexture(self: Self) -> str:
+        return self.object.by_member('ButtonTexture').value
+
+    @ButtonTexture.setter
+    def ButtonTexture(self: Self, value: str) -> None:
+        edit.member(self.object, 'ButtonTexture', value)
+
+    @property
+    def CountryTexture(self: Self) -> str:
+        return self.object.by_member('CountryTexture').value
+
+    @CountryTexture.setter
+    def CountryTexture(self: Self, value: str) -> None:
+        edit.member(self.object, 'CountryTexture', value)
+
+    @property
+    def DisplayRoadSpeedInKmph(self: Self) -> float:
+        return self.object.by_member('DisplayRoadSpeedInKmph').value
+
+    @DisplayRoadSpeedInKmph.setter
+    def DisplayRoadSpeedInKmph(self: Self, value: float) -> None:
+        edit.member(self.object, 'DisplayRoadSpeedInKmph', value)
+
+    @property
+    def GenerateName(self: Self) -> bool:
+        return self.object.by_member('GenerateName').value
+
+    @GenerateName.setter
+    def GenerateName(self: Self, value: bool) -> None:
+        edit.member(self.object, 'GenerateName', value)
+
+    @property
+    def InfoPanelConfigurationToken(self: Self) -> str:
+        return self.object.by_member('InfoPanelConfigurationToken').value
+
+    @InfoPanelConfigurationToken.setter
+    def InfoPanelConfigurationToken(self: Self, value: str) -> None:
+        edit.member(self.object, 'InfoPanelConfigurationToken', value)
+
+    @property
+    def MenuIconTexture(self: Self) -> str:
+        return self.object.by_member('MenuIconTexture').value
+
+    @MenuIconTexture.setter
+    def MenuIconTexture(self: Self, value: str) -> None:
+        edit.member(self.object, 'MenuIconTexture', value)
+
+    @property
+    def NameToken(self: Self) -> str:
+        return self.object.by_member('NameToken').value
+
+    @NameToken.setter
+    def NameToken(self: Self, value: str) -> None:
+        edit.member(self.object, 'NameToken', value)
+
+    @property
+    def SpecialtiesList(self: Self) -> list[str]:
+        return self.object.by_member('SpecialtiesList').value
+
+    @SpecialtiesList.setter
+    def SpecialtiesList(self: Self, value: list[str]) -> None:
+        edit.member(self.object, 'SpecialtiesList', value)
+
+    @property
+    def TypeStrategicCount(self: Self) -> str:
+        return self.object.by_member('TypeStrategicCount').value
+
+    @TypeStrategicCount.setter
+    def TypeStrategicCount(self: Self, value: str) -> None:
+        edit.member(self.object, 'TypeStrategicCount', value)
+
+    @property
+    def UnitRole(self: Self) -> str:
+        return self.object.by_member('UnitRole').value
+
+    @UnitRole.setter
+    def UnitRole(self: Self, value: str) -> None:
+        edit.member(self.object, 'UnitRole', value)
+
+    @property
+    def UpgradeFromUnit(self: Self) -> str | None:
+        return self.object.by_member('UpgradeFromUnit').value
+
+    @UpgradeFromUnit.setter
+    def UpgradeFromUnit(self: Self, value: str | None) -> None:
+        edit.member(self.object, 'UpgradeFromUnit', value)
