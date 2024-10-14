fb96baf42e0c23779fa2075eb02dcd7f05413d21
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-10-13 21:16:07 -0600

    WeaponDepictionOperator model probably done

---
+++ script/model/depiction_operators/weapon/weapon_missile_carriage.py
@@ -0,0 +1,23 @@
+from typing import Self
+
+import utils.ndf.ensure as ensure
+from ndf_parse.model import Object
+
+from ._abc import WeaponDepictionOperator
+
+
+class DepictionOperator_WeaponMissileCarriageFire(WeaponDepictionOperator):
+    def __init__(self: Self, index: int, connoisseur_name: str, weapon_index: int | None = None):
+        super().__init__(index)
+        self.connoisseur_name = connoisseur_name
+        self.weapon_index = weapon_index if weapon_index is not None else index
+
+    def to_ndf(self: Self) -> Object:
+        return ensure._object(
+            'DepictionOperator_WeaponMissileCarriageFire',
+            Connoisseur=self.connoisseur_name,
+            FireEffectTag = ensure.quoted(f"weapon_effet_tag{self.index}"),
+            NbProj=1,
+            WeaponIndex=self.weapon_index,
+            WeaponShootDataPropertyName=[ensure.quoted(f'WeaponShootData_0_{self.index}')]
+        )
