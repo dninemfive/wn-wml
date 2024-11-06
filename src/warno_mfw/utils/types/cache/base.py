1b4de79b2ad85df3c2ab1c796b479c03395dcc99
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-11-06 15:25:28 -0700

    move _accessed to FileCache

--- src/warno_mfw/utils/types/cache/base.py
+++ src/warno_mfw/utils/types/cache/base.py
@@ -8,28 +8,20 @@ V = TypeVar('V')
 class BaseCache(Generic[V]):
     def __init__(self: Self,):
         self._data: dict[str, V] = None
-        self._accessed: defaultdict[str, bool] = defaultdict(lambda: False)
 
     def __getitem__(self: Self, key: str) -> V:
-        self._accessed[key] = True
         return self._data[key]
     
     def __setitem__(self: Self, key: str, val: V):
-        self._accessed[key] = True
         self._data[key] = val
 
     def __contains__(self: Self, key: str) -> bool:
-        self._accessed[key] = True
         return key in self._data
 
     def load(self: Self, parent_msg: Message | None = None) -> None:
         raise NotImplemented
 
     def save(self: Self, parent_msg: Message | None = None) -> None:
-        with try_nest(parent_msg, self.file_path) as _:
-            self._save_data({k: v for k, v in self._data.items() if self._accessed[k]})
-
-    def _save_data(self: Self, accessed_items: dict[str, V]) -> None:
         raise NotImplemented
 
     @property
--- src/warno_mfw/utils/types/cache/file.py
+++ src/warno_mfw/utils/types/cache/file.py
@@ -1,7 +1,9 @@
 import os
+from collections import defaultdict
 from typing import Self, TypeVar
 
 from warno_mfw.utils.io import load_file, write_file
+from warno_mfw.utils.types.message import Message, try_nest
 
 from .base import BaseCache
 
@@ -11,9 +13,24 @@ DEFAULT_FOLDER = rf"script\_cache"
 class FileCache(BaseCache[V]):
     def __init__(self: Self,  name: str, folder: str = DEFAULT_FOLDER):
         self.file_path = os.path.join(folder, f'{name}.cache')
+        self._accessed: defaultdict[str, bool] = defaultdict(lambda: False)        
 
-    def load(self: Self) -> dict[str, V]:
-        return load_file(self.file_path, {})
+    def __getitem__(self: Self, key: str) -> V:
+        self._accessed[key] = True
+        return super().__getitem__(key)
+    
+    def __setitem__(self: Self, key: str, val: V):
+        self._accessed[key] = True
+        super().__setitem__(key, val)
 
-    def _save_data(self: Self, accessed_items: dict[str, V]) -> None:
-        write_file(accessed_items, self.file_path)
+    def __contains__(self: Self, key: str) -> bool:
+        self._accessed[key] = True
+        return super().__contains__(key)
+
+    def load(self: Self, parent_msg: Message | None) -> dict[str, V]:
+        with try_nest(parent_msg, self.file_path) as _:
+            self._data = load_file(self.file_path, {})
+
+    def save(self: Self, parent_msg: Message | None) -> None:
+        with try_nest(parent_msg, self.file_path) as _:
+            write_file({k: v for k, v in self._data.items() if self._accessed[k]}, self.file_path)
