4bcf843110ed68f345d99f6edc979d4a0fa37751
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-11-08 12:28:17 -0700

    idk

--- src/warno_mfw/hints/_generate/_generate_folder_paths.py
+++ src/warno_mfw/hints/_generate/_generate_folder_paths.py
@@ -1,10 +1,43 @@
 import os
 import shutil
-from typing import Iterable
+from typing import Iterable, Self
 
 def _make_init(path: str, lines: Iterable[str]) -> None:
         with open(os.path.join(path, '__init__.py'), 'w') as file:
             file.write('\n'.join(lines))
+
+def _ensure_valid_var_name(variable_name: str) -> str:
+    if variable_name.startswith(tuple(str(x) for x in range(10))):
+        return f'_{variable_name}'
+    return variable_name
+
+class Folder(object):
+    def __init__(self: Self, path: str):
+        self.path = path
+        for _, dirs, files in os.walk(path):
+            self.dirs = dirs
+            self.files = files
+            break
+
+    @property
+    def init_lines(self: Self) -> Iterable[str]:
+        if any(self.files):
+            yield 'from typing import Literal\n'
+        yield from [f'from {x} import *' for x in self.dirs]
+        if any(self.dirs):
+            yield ''
+        for file in self.files:
+            variable_name = _ensure_valid_var_name(os.path.splitext(file)[0])
+            file_path = os.path.join(self.path, file).replace('\\', '/')
+
+    def generate(self: Self) -> str | None:
+        ...
+        # create folder
+        # for each folder, call generate
+        # write __init__.py
+
+def make_folder(path: str, must_be_in: str) -> Folder | None:
+    pass
 
 def generate_module_for_folder(src_path: str, output_path: str, generated_only: bool) -> None:
     print(output_path)
