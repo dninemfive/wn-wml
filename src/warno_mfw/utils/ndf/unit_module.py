9618a68f7ead79a9846ceb1ba353bfce0a5e9119
dninemfive <11084010+dninemfive@users.noreply.github.com>
2024-11-21 16:28:48 -0700

    started reworking module finding but that's probably out-of-scope for this

--- src/warno_mfw/utils/ndf/unit_module.py
+++ src/warno_mfw/utils/ndf/unit_module.py
@@ -1,43 +1,69 @@
-from typing import Callable
+from typing import Callable, Literal
 from ndf_parse import Mod
 from ndf_parse.model import List, ListRow, Map, MapRow, MemberRow, Object
 from ndf_parse.model.abc import CellValue
 from warno_mfw.utils.ndf import ensure
 
 MODULES_DESCRIPTORS = "ModulesDescriptors"
+T_MODULE_SELECTOR = "TModuleSelector"
+_GetMode = Literal['by type', 'by name', 'by default']
 
 def get_modules_descriptors(unit_or_modules: Object | List) -> List:
     if isinstance(unit_or_modules, List):
         return unit_or_modules
     return unit_or_modules.by_member(MODULES_DESCRIPTORS).value
 
-def get_row(unit_or_modules: Object | List, type_or_name: str, by_name: bool) -> ListRow:
+def _get_row_by_name(modules: List, name: str) -> ListRow:
+    return modules.by_name(name)
+
+def _get_row_by_type(modules: List, type: str) -> ListRow | None:
+    for module in modules.match_pattern(f'{type}()'):
+        return module
+    return None
+
+def _get_row_by_selector_default_type(modules: List, type: str) -> ListRow | None:
+    for module in modules.match_pattern(f'{T_MODULE_SELECTOR}()'):
+        try:
+            if module.value.by_member('Default').type == type:
+                return module
+        except:
+            continue
+    return None
+
+_GET_MODES: dict[_GetMode, Callable[[List, str], ListRow | None]] = {
+    'by type': _get_row_by_type,
+    'by name': _get_row_by_name,
+    'by default': _get_row_by_selector_default_type
+}
+
+def get_row(unit_or_modules: Object | List, type_or_name: str, mode: _GetMode) -> ListRow:
     result: ListRow | None = None
     modules = get_modules_descriptors(unit_or_modules)
-    if by_name:
-        result = modules.by_name(type_or_name)
+    if mode in _GET_MODES:
+        result = _GET_MODES[mode](modules, type_or_name)
     else:
-        for module in modules.match_pattern(f'{type_or_name}()'):
-            result = module
-            break
+        raise Exception(f'Unrecognized modules.get_row mode: {mode}!')
     if result is None:
-        report = f"Could not find module {type_or_name}{" by name" if by_name else ""} on "
+        report = f"Could not find module {type_or_name} {mode} on "
         report += f"unit {unit_or_modules.by_member('ClassNameForDebug').value}" if isinstance(unit_or_modules, Object) else f'list {str(unit_or_modules[:32])}'
         raise KeyError(report)
     return result
 
-def get(unit_or_modules: Object | List, module_type: str) -> ListRow | None:
-    result: ListRow | None = None
-    for module in get_modules_descriptors(unit_or_modules).match_pattern(f'{module_type}()'):
-        result = module
-        break
-    return result
+def get_selector(unit_or_modules: Object | List, default_type: str, selector_type: str = 'TModuleSelector') -> ListRow:
+    for module in get_modules_descriptors(unit_or_modules).match_pattern(f'{selector_type}()'):
+        try:
+            default: Object = module.value
+            if default.type == default_type:
+                return module
+        except:
+            continue
+    raise Exception(f'Could not find {selector_type} with Default.type == {default_type}!')
 
-def get_index(unit_or_modules: Object | List, type_or_name: str, by_name: bool = False) -> int:
-    return get_row(unit_or_modules, type_or_name, by_name).index
+def get_index(unit_or_modules: Object | List, type_or_name: str, mode: _GetMode = 'by type') -> int:
+    return get_row(unit_or_modules, type_or_name, mode).index
 
-def get(unit_or_object: Object | List, type_or_name: str, by_name: bool = False) -> Object:
-    return get_row(unit_or_object, type_or_name, by_name).value
+def get(unit_or_object: Object | List, type_or_name: str, mode: _GetMode = 'by type') -> Object:
+    return get_row(unit_or_object, type_or_name, mode).value
 
 def replace(unit_or_modules: Object | List, value: CellValue, type_or_name: str, by_name: bool = False) -> None:
     get_row(unit_or_modules, type_or_name, by_name).value = value
